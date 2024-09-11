da5f99d776a7356df72ec9b89692f31e102c1f6d
정승훈 <seunghun311@naver.com>
2024-09-11 09:58:05 +0900

    Feat: add init frontend project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,20 @@
-import './App.css'
+import React from 'react';
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
 import Layout from './components/layouts/Layout'
 import Main from './pages/Main'
 
 function App() {
-
   return (
     <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Main />} />
-        </Routes>
-      </Layout>
-    </Router>
-  )
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </Layout>
+  </Router>
+  );
 }
 
-export default App
+export default App;
