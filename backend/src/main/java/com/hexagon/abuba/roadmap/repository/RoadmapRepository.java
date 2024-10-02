d6dceca9b29c02348812e38794420dde112bc56d
김재윤 <joajy98@gmail.com>
2024-10-02 11:11:41 +0900

    feat: add repository's information call methods

--- backend/src/main/java/com/hexagon/abuba/roadmap/repository/RoadmapRepository.java
+++ backend/src/main/java/com/hexagon/abuba/roadmap/repository/RoadmapRepository.java
@@ -2,6 +2,22 @@ package com.hexagon.abuba.roadmap.repository;
 
 import com.hexagon.abuba.roadmap.Roadmap;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface RoadmapRepository extends JpaRepository<Roadmap, Long> {
+
+    @Query(value = "SELECT r FROM Roadmap r WHERE r.startDate >= :birthDay")
+    List<Roadmap> findSomeInformation(int birthDay);
+
+    @Query(value = "SELECT r FROM Roadmap r WHERE r.startDate >= :birthDay and r.type = 'HEALTH'")
+    List<Roadmap> findHealthInformationByBirth(int birthDay);
+
+    @Query(value = "SELECT r FROM Roadmap r WHERE r.startDate >= :birthDay and r.type = 'GROWTH'")
+    List<Roadmap> findAverageBehaviorInformationByBirth(int birthDay);
+
+    @Query(value = "SELECT r FROM Roadmap r WHERE r.startDate >= :birthDay and r.type = 'SUPPORT'")
+    List<Roadmap> findSupportInformationByBirth(int birthDay);
+
 }
