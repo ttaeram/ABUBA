5c9cf63348b17040a8a2f75f07edc9e049dcf1cd
김재윤 <joajy98@gmail.com>
2024-10-02 11:27:49 +0900

    feat: change query to recommend better fit, add limit items count

--- backend/src/main/java/com/hexagon/abuba/roadmap/repository/RoadmapRepository.java
+++ backend/src/main/java/com/hexagon/abuba/roadmap/repository/RoadmapRepository.java
@@ -8,16 +8,16 @@ import java.util.List;
 
 public interface RoadmapRepository extends JpaRepository<Roadmap, Long> {
 
-    @Query(value = "SELECT r FROM Roadmap r WHERE r.startDate >= :birthDay")
+    @Query(value = "SELECT r FROM Roadmap r WHERE r.endDate >= :birthDay order by r.startDate limit 15")
     List<Roadmap> findSomeInformation(int birthDay);
 
-    @Query(value = "SELECT r FROM Roadmap r WHERE r.startDate >= :birthDay and r.type = 'HEALTH'")
+    @Query(value = "SELECT r FROM Roadmap r WHERE r.endDate >= :birthDay and r.type = 'HEALTH' order by r.startDate limit 5")
     List<Roadmap> findHealthInformationByBirth(int birthDay);
 
-    @Query(value = "SELECT r FROM Roadmap r WHERE r.startDate >= :birthDay and r.type = 'GROWTH'")
+    @Query(value = "SELECT r FROM Roadmap r WHERE r.endDate >= :birthDay and r.type = 'GROWTH' order by r.startDate limit 5")
     List<Roadmap> findAverageBehaviorInformationByBirth(int birthDay);
 
-    @Query(value = "SELECT r FROM Roadmap r WHERE r.startDate >= :birthDay and r.type = 'SUPPORT'")
+    @Query(value = "SELECT r FROM Roadmap r WHERE r.endDate >= :birthDay and r.type = 'SUPPORT' order by r.startDate limit 5")
     List<Roadmap> findSupportInformationByBirth(int birthDay);
 
 }
