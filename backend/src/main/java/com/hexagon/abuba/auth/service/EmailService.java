91e3c3f99e14c5e95ac5a80bbef02635b9c0abdc
hoiae <donald819@naver.com>
2024-10-07 10:42:44 +0900

    temp

--- backend/src/main/java/com/hexagon/abuba/auth/service/EmailService.java
+++ backend/src/main/java/com/hexagon/abuba/auth/service/EmailService.java
@@ -3,6 +3,7 @@ package com.hexagon.abuba.auth.service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
