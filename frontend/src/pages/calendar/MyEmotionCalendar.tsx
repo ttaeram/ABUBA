9882d0fa7fd1b9702f8e0b6c641e18a8cff122e2
정승훈 <seunghun311@naver.com>
2024-10-10 15:00:32 +0900

    feat: 감정 달력 구현 (#45)

--- frontend/src/pages/calendar/MyEmotionCalendar.tsx
+++ frontend/src/pages/calendar/MyEmotionCalendar.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import EmotionCalendar from '../../components/calendar/EmotionCalendar'
+import styled from 'styled-components';
 
 const MyEmotionCalendar = () => {
   return (
     <div>
+      <TextContainer>
+        <TitleText>
+          육아 감정 일기
+        </TitleText>
+        <SubText>
+          일기 작성 당시의 감정을 AI가 분석해줘요!
+        </SubText>
+      </TextContainer>
       <EmotionCalendar/>
     </div>
   
@@ -11,3 +20,28 @@ const MyEmotionCalendar = () => {
 }
 
 export default MyEmotionCalendar
+
+const TextContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  padding: 20px;
+  flex-direction: column;
+  color:#acacac;
+
+`
+
+const TitleText = styled.div`
+  display: flex;
+  font-size: 22px;
+  color:black;
+  margin-bottom: 5px;
+`
+
+const SubText = styled.div`
+  display: flex;
+  font-size:14px;
+  margin-bottom: 10px;
+  justify-content: space-between;
+
+`
