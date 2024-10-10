9c6b7de8596b9d5db50422256c6abcb84fbf1f04
정승훈 <seunghun311@naver.com>
2024-10-11 08:30:55 +0900

    feat: 사전 정보 입력 페이지 전환 효과 (#45)

--- frontend/src/pages/OnBoardingInfoPage.tsx
+++ frontend/src/pages/OnBoardingInfoPage.tsx
@@ -8,7 +8,7 @@ import ParentAccount from '../components/onboardinginfo/ParentAccountForm';
 
 const OnboardingInfoPage = () => {
   const [step, setStep] = useState(1);
-  const [fade, setFade] = useState(false);  // 페이드 애니메이션 상태 추가
+  const [fade, setFade] = useState(false); 
   const navigate = useNavigate();
 
   const handleNextStep = (): void => {
@@ -16,7 +16,7 @@ const OnboardingInfoPage = () => {
     setTimeout(() => {
       setStep((prevStep) => prevStep + 1);
       setFade(false);
-    }, 300);  // 300ms 후에 컴포넌트 전환
+    }, 300); 
   };
 
   const handlePreviousStep = (): void => {
@@ -24,7 +24,7 @@ const OnboardingInfoPage = () => {
     setTimeout(() => {
       setStep((prevStep) => prevStep - 1);
       setFade(false);
-    }, 300);  // 300ms 후에 컴포넌트 전환
+    }, 300);  
   };
 
   const handleComplete = (): void => {
@@ -60,7 +60,6 @@ const OnboardingInfoPage = () => {
 
 export default OnboardingInfoPage;
 
-// 페이드 애니메이션 정의
 const fadeIn = keyframes`
   from {
     opacity: 0;
@@ -79,12 +78,11 @@ const fadeOut = keyframes`
   }
 `;
 
-// CenteredFadeWrapper에 가운데 정렬 적용
 const CenteredFadeWrapper = styled.div<{ fade: boolean }>`
   animation: ${({ fade }) => (fade ? fadeOut : fadeIn)} 300ms ease-in-out;
   width: 100%;
   display: flex;
-  justify-content: center;  // 수평 가운데 정렬
-  align-items: center;      // 수직 가운데 정렬
+  justify-content: center;  
+  align-items: center;      
   flex-direction: column;
 `;
